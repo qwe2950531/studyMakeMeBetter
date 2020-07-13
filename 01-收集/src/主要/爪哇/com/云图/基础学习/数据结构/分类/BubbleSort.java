c709266935773bb43ffa4a15ea1e17da2ab07f84
qwe2950531 <790180627@qq.com>
2020-07-13 16:30:52 +0800

    Update and rename 01-collection/src/main/java/com/yuntu/basis_learn/data_structure/sort/BubbleSort.java to 01-收集/src/主要/爪哇/com/云图/基础学习/数据结构/分类/BubbleSort.java

--- 01-收集/src/主要/爪哇/com/云图/基础学习/数据结构/分类/BubbleSort.java
+++ 01-收集/src/主要/爪哇/com/云图/基础学习/数据结构/分类/BubbleSort.java
@@ -9,7 +9,7 @@ public class BubbleSort {
 		this.arr = arr;
 	}
 	public void display(){
-		for(int i=0;i<arr.length;i++){
+		for(int i=0-0;i<arr.length;i++){
 			System.out.print(arr[i]+"  ");
 		}
 		System.out.println();
